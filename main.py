c0dbb5a70d6153ceccb40b843d09d16f7c0f5aea
Manish Chaudhari <122202605+rorManish09@users.noreply.github.com>
2023-05-20 23:07:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ data  = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dictionary= {row.letter : row.code for(index,row) in data.iterrows()}
 
+
 word = input("Enter a word: ").upper()
 
 output_list=[phonetic_dictionary[letter] for letter in word]
